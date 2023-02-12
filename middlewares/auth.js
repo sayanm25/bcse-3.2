69da4e978251052be701d6a4d092df54773475f1
sayanm25 <81901355+sayanm25@users.noreply.github.com>
2023-02-13 03:43:54 +0530

    Create auth.js

---
+++ middlewares/auth.js
@@ -0,0 +1,40 @@
+class AuthVer{
+    static isLogin = async(req,res,next)=>{
+        try{
+            if(req.session.user_id){}
+            else {
+
+                res.redirect('/login')
+                return;
+            }
+            next();
+        } catch (err) {
+            console.log(err);
+        }
+        
+    }
+    static isLogout = async(req,res,next)=>{
+        try{
+            if(req.session.user_id){
+                res.redirect('/dashboard')
+                return;
+            }
+            
+            
+            next();
+        } catch (err) {
+            console.log(err);
+        }
+        
+    }
+    static logout = async(req,res)=>{
+        try{
+            req.session.destroy();
+            res.redirect('/login')
+        } catch (err) {
+            console.log(err);
+        }
+        
+    } }
+
+ export default AuthVer
